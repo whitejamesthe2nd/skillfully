9e9867d1e4c6dc957d30fc9b86cbc62aabcb83f2
James White <whitejamesthe2nd@gmail.com>
2020-11-10 16:28:55 -0500

    added some form styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,9 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { login } from "../redux/actions/AuthActions";
+import { makeStyles } from "@material-ui/core";
+
+
 
 function Login(props) {
     const [email, setEmail] = useState('ian@aa.io');
@@ -8,27 +11,67 @@ function Login(props) {
     console.log(password)
     const dispatch = useDispatch()
 
+    const useStyles = makeStyles({
+        loginDiv: {
+            display:'flex',
+            justifyContent:'center'
+        },
+        form:{
+            dipslay: 'grid',
+            gridTemplateRows: '1fr 1fr 1fr',
+            fontSize: '24px'
+        },
+        email:{
+            gridRowStart: '2',
+            width:'200px',
+            height: '25px',
+            fontSize: '16px',
+            borderRadius:'5px'
+        },
+        password:{
+            width:'200px',
+            height: '25px',
+            fontSize: '16px',
+            borderRadius:'5px'
+
+        },
+        submit:{
+            width: '100px',
+            height: '25px',
+            backgroundColor:'white',
+            borderRadius:'5px'
+        }
+    })
+
+    const classes = useStyles();
 
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(login(email,password))
+        dispatch(login(email, password))
     }
-    const handleChangePassword = (e) =>{
-         setPassword(e.target.value)
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value)
     }
-    const handleChangeEmail = (e) =>{
+    const handleChangeEmail = (e) => {
         setEmail(e.target.value)
     }
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <label>Email</label>
-            <input type="email" value={'ian@aa.io'} onChange={handleChangeEmail}></input>
-            <label>Password</label>
-            <input type='password' value={'password'} onChange={handleChangePassword}></input>
-            <button type='submit'>Submit</button>
-        </form>
+            <div className={classes.loginDiv}>
+                <form onSubmit={handleSubmit} className={classes.form}>
+                <div>
+                <h3>Welcome To Skillfully Please Login</h3>
+                </div>
+                    <div >
+                        <input type="email" value={'ian@aa.io'} className={classes.email} onChange={handleChangeEmail}></input>
+                    </div>
+                    <div >
+                        <input type='password' value={'password'} className={classes.password} onChange={handleChangePassword}></input>
+                    </div>
+                    <button type='submit' className={classes.submit}>Submit</button>
+                </form>
+            </div>
 
         </>
     );
